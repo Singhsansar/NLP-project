3775ce5d7ec363d40195198bb58fc4a586b5b338
Nikhil Singh <92269431+Singhsansar@users.noreply.github.com>
2023-12-24 21:09:09 +0000

    added requirements

--- nlplogic/corenlp.py
+++ nlplogic/corenlp.py
@@ -3,3 +3,9 @@ import wikipedia
 
 golden_state_warriors_text = wikipedia.summary("Golden State Warriors")
 print(golden_state_warriors_text)
+blob = TextBlob(golden_state_warriors_text)
+print(blob.tags)
+print(blob.noun_phrases)
+print(blob.sentiment)
+print(blob.sentences)
+print(blob.words)
