6d1eb8168f60e3d348779f3e05c42942d0164de5
Nikhil Singh <92269431+Singhsansar@users.noreply.github.com>
2023-12-24 22:13:24 +0000

    removed unused import

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,4 +1,5 @@
-from nlplogic.corenlp import get_phrases, search_wiki, get_wiki_summary, text_blob
-import fire 
+from nlplogic.corenlp import get_phrases
+
+
 def test_get_phrases():
-    assert 'golden state' in get_phrases("Golden State Warriors") 
+    assert "golden state" in get_phrases("Golden State Warriors")
